c6f188d4ad99d7be07435cad95dd2008e8c98837
Dhirendra Gohil <dhigohil@publicisgroupe.net>
2024-02-26 12:56:31 +0530

    Initial commit

---
+++ src/main/java/org/learning/JenkinsCalculator.java
@@ -0,0 +1,13 @@
+package org.learning;
+
+public class JenkinsCalculator {
+
+    public int addNumbers(int first, int second) {
+        return first + second;
+    }
+
+    public int subtractNumbers(int first, int second) {
+        return first - second;
+    }
+
+}
