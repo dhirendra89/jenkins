b9874a5a9935b7c2adbe6902e03f3328ec37665d
Dhirendra <39556023+dhirendra89@users.noreply.github.com>
2024-02-26 13:20:37 +0530

    Update JenkinsCalculator.java

--- src/main/java/org/learning/JenkinsCalculator.java
+++ src/main/java/org/learning/JenkinsCalculator.java
@@ -2,8 +2,8 @@ package org.learning;
 
 public class JenkinsCalculator {
 
-    public int addNumbers(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
+    public int addNumbers(int first, int second) {
+        return first + second;
     }
 
     public int subtractNumbers(int first, int second) {
